c1a119a93b6002da94e675e8370287d9859a3368
Gabryell568 <gabryellpaulo568@gmail.com>
2023-11-15 07:23:47 -0300

    Projeto da Dio

---
+++ index.js
@@ -0,0 +1,21 @@
+let nome = "Roger"
+let Nxp = 6000
+let Elo =["Ferro","Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+
+if (Nxp <= 999){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[0])
+} else if (Nxp == 1000){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[1])
+} else if (Nxp == 2000){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[2])
+} else if (Nxp == 3000){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[3])
+} else if (Nxp == 4000){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[4])
+} else if (Nxp == 5000){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[5])
+} else if (Nxp == 6000){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[6])
+} else if (Nxp == 7000){
+    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[7])
+}
