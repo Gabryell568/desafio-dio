8f3e155153845935405dd196e743ccf6c04c7788
Gabryell568 <gabryellpaulo568@gmail.com>
2023-11-22 21:09:18 -0300

    atualizei as estruturas de controle do código

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let nome = "Roger"
-let Nxp = 6000
+let nome = 'Roger'
+let Nxp = 100000
 let Elo =["Ferro","Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
 
-if (Nxp <= 999){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[0])
-} else if (Nxp == 1000){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[1])
-} else if (Nxp == 2000){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[2])
-} else if (Nxp == 3000){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[3])
-} else if (Nxp == 4000){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[4])
-} else if (Nxp == 5000){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[5])
-} else if (Nxp == 6000){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[6])
-} else if (Nxp == 7000){
-    console.log("O heroi " +nome+ " que está com " + Nxp+ " de XP e está no Elô "+ Elo[7])
+if (Nxp > 0 && Nxp < 1100){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[0]}`)
+} else if (Nxp > 1101 && Nxp < 2000){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[1]}`)
+}else if (Nxp > 2001 && Nxp < 5000){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[2]}`)
+} else if (Nxp > 6001 && Nxp < 7000){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[3]}`)
+}else if (Nxp > 7001 && Nxp < 8000){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[4]}`)
+}else if (Nxp > 8001 && Nxp < 9000){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[5]}`)
+}else if (Nxp > 9001 && Nxp < 10000){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[6]}`)
+}else if (Nxp >= 10001){
+    console.log(`o heroi ${nome} está com ${Nxp} de XP total e atualmente está no Elô ${Elo[7]}`)
 }
